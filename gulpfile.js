453224cadfed97a72ecf72d7f4ac24d0e8608eba
ChugunovRoman <Zebs-BMK@yandex.ru>
2018-01-01 21:06:59 +0300

    Clean project

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 'use strict';
-const config = require('./config');
 
 let gulp     = require('gulp'),
     watch    = require('gulp-watch'),
@@ -11,26 +10,37 @@ let gulp     = require('gulp'),
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant');
 
+let path =  {
+    'build': {
+        'css': `${__dirname}/public/css`,
+        'img': `${__dirname}/public/img`
+    },
+    'src': {
+        'css': `${__dirname}/public/sass`,
+        'img': `${__dirname}/public/i`
+    }
+}
+
 // Таск для сборки и оптимизации стилий
 // Добавляет префиксы и сжимает css
 gulp.task('style:build', () => {
 
-    gulp.src(config.gulp.src.css)
+    gulp.src(path.src.css)
         .pipe(sass())
         .pipe(prefixer())
         .pipe(cssmin())
-        .pipe(gulp.dest(config.gulp.build.css));
+        .pipe(gulp.dest(path.build.css));
 });
 
 // Таск для сжатия картинок
 gulp.task('img:minify', () => {
-    gulp.src(config.gulp.src.img)
+    gulp.src(path.src.img)
         .pipe(imagemin())
-        .pipe(gulp.dest(config.gulp.build.img));
+        .pipe(gulp.dest(path.build.img));
 });
 
 gulp.task('watch', () => {
-    watch(config.gulp.watch.css, (event, cb) => {
+    watch(path.watch.css, (event, cb) => {
         gulp.start('style:build');
     });
 });
